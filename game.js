3b0bf5c74e80127c9b7fd04df78101740d7d8e30
Don <Dcdegr59@Harrisonburg.k12.va.us>
2019-09-17 09:13:29 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,33 @@
-var score = 0;
+var score = -9000000;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress);
-changeTitle('Clicker Game');
+createButton('Click me', btnPress);
+changeTitle('Click the Button');
 
 setInterval(btnPress, 1000);
+
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+var score = -9000000;
+
+postToPage(score); // Print to the page
+
+createButton('Click me', btnPress);
+changeTitle('Click the Button');
+
+setInterval(btnPress, 1000);
+
+
+
+
 
 function btnPress() {
   score++;
