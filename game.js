4937ce9d03b251838bcd0183cf404e6b94db750b
Don <Dcdegr59@Harrisonburg.k12.va.us>
2019-09-18 08:33:51 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
-var score = -9000000;
+var score = 0; 
 
 postToPage(score); // Print to the page
 
 createButton('Click me', btnPress);
 changeTitle('Click the Button');
 
-setInterval(btnPress, 1000);
-
+//setInterval(btnPress, 1000);
 
 
 
@@ -16,15 +15,17 @@ function btnPress() {
   postToPage(score);
 }
 
-var score = -9000000;
+
+
+
+var score = 5; 
 
 postToPage(score); // Print to the page
 
-createButton('Click me', btnPress);
+createButton("5", btnPress);
 changeTitle('Click the Button');
 
-setInterval(btnPress, 1000);
-
+//setInterval(btnPress, 1000);
 
 
 
@@ -33,4 +34,187 @@ function btnPress() {
   score++;
   postToPage(score);
 }
+
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("10", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("15", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("20", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("25", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("30", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("45", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("50", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("75", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+
+var score = 0; 
+
+postToPage(score); // Print to the page
+
+createButton("100", btnPress);
+changeTitle('Click the Button');
+
+//setInterval(btnPress, 1000);
+
+
+
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
+
+
+
+
+
+
+
+
 
