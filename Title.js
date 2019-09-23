fb487f36ad37c252c37cd5a557d455f5b68ef78d
Don <Dcdegr59@Harrisonburg.k12.va.us>
2019-09-23 09:56:42 -0400

    I tried to make him walk

---
+++ Title.js
@@ -0,0 +1,21 @@
+var score = 0; // Create a variable to store the score in
+
+var title = new Title('Train (Working title (Prototype))'); // Create a title element
+var button = new Button('Walk', btnPress);   // Create a button element
+var hidden = new Button('Pick up random item');
+hidden.hide();
+var scr = new Text(score);                    // Create a text element
+
+setInterval(btnPress, 1000);   // Set up a loop
+
+function btnPress() {
+  score++;         // Increase the score
+  scr.edit(score); // Update the page with the new score
+  if(score > 10) {
+    hidden.show(250);
+  }
+  if(score > 12) {
+    hidden.hide(500);
+  }
+}
+
