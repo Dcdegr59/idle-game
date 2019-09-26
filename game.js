65276e6d2b60982d0e024bf0ec8a17bac6d8704a
Don <Dcdegr59@Harrisonburg.k12.va.us>
2019-09-26 09:54:19 -0400

    Added in 2 random functions

--- game.js
+++ game.js
@@ -1,17 +1,24 @@
 var score = 0; // Create a variable to store the score in
+var scoreIsHidden = false;
 
 var title = new Title('Train (Working title (Prototype))'); // Create a title element
 var button = new Button('Walk', btnPress);   // Create a button element
 var hidden = new Button('Use weapon');
-var timer = new Button ('Check timer')
+var timer = new Button ('Check timer', timerCheck)
 var random = new Button ('Open crate')
-var random = new Button ('Pick up random item')
+var random2 = new Button ('Pick up random item')
+var scr = new Text(score);     
 
 
-
-var scr = new Text(score);                    // Create a text element
 //changes
 setInterval(btnPress, 1000);   // Set up a loop
+
+//var loop = setInterval(draw,5);
+var btn = new button ('Stop',stopLoop);
+
+function stopLoop() {
+  clearInterval(loop);
+}
 
 function btnPress() {
   score++;         // Increase the score
@@ -20,6 +27,23 @@ function btnPress() {
     hidden.show(250);
   }
   if(score > 12) {
-    hidden.hide(250);
+    hidden.hide(9000);
   }
+}
+function timerCheck() {
+  if(scoreIsHidden == false) {
+    scr.hide();
+    scoreIsHidden = true;
+  } else {
+    scr.show();
+    scoreIsHidden = false;
+  }
+ 
+}
+function randomCrate() {
+
+}
+
+function randomItem() {
+
 }
